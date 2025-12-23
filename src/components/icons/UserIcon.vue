132ff950792c115d98f86e82d1cbe0b70133ba20
Jideobi Ofomah <osdevseries@gmail.com>
2025-12-23 06:59:06 +0100

    feature: Add UserIcon svg and component

---
+++ src/components/icons/UserIcon.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="12" cy="8" r="3.5" stroke="currentColor" stroke-width="1" stroke-linecap="round"
+            stroke-linejoin="round" />
+        <path d="M18.5 20C18.5 16.96 15.58 14.5 12 14.5C8.41 14.5 5.5 16.96 5.5 20" stroke="currentColor"
+            stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,17 +1,19 @@
-import HomeIcon from './HomeIcon.vue';
-import CardIcon from './CardIcon.vue';
-import ChatIcon from './ChatIcon.vue';
-import MenuIcon from './MenuIcon.vue';
-import PaymentIcon from './PaymentIcon.vue';
-import QrIcon from './QrIcon.vue';
-import TransferIcon from './TransferIcon.vue';
+import HomeIcon from "./HomeIcon.vue";
+import CardIcon from "./CardIcon.vue";
+import ChatIcon from "./ChatIcon.vue";
+import MenuIcon from "./MenuIcon.vue";
+import PaymentIcon from "./PaymentIcon.vue";
+import QrIcon from "./QrIcon.vue";
+import TransferIcon from "./TransferIcon.vue";
+import UserIcon from "./UserIcon.vue";
 
 export const icons = {
-    card: CardIcon,
-    chat: ChatIcon,
-    home: HomeIcon,
-    menu: MenuIcon,
-    payment: PaymentIcon,
-    qr: QrIcon,
-    transfer: TransferIcon
+  card: CardIcon,
+  chat: ChatIcon,
+  home: HomeIcon,
+  menu: MenuIcon,
+  payment: PaymentIcon,
+  qr: QrIcon,
+  transfer: TransferIcon,
+  user: UserIcon,
 };
